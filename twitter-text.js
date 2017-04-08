5cf4e033ceda55681c24fc21fcb5dc281ad146fb
sapphire <sapphiredev@users.noreply.github.com>
2017-04-09 01:50:39 +0900

    base structure

---
+++ twitter-text.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class TwitterText extends Component {
+	render() {
+		return (
+			<Text>test</Text>
+		);
+	}
+};
